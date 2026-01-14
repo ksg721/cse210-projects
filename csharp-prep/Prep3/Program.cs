e7cfdfd7567bad7a0b24b7a46bfe15e84e493da4
ksg721 <gre20018@byui.edu>
2026-01-13 21:20:15 -0700

    Completed prep 3 number guessinggame

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+        int guess;
+
+        Console.WriteLine("Welcome to the Magic Number Guessing Game!");
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+        } while (guess != magicNumber);
+        Console.WriteLine("You guessed it!");
+
     }
 }
