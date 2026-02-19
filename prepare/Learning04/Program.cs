5b1a254e9381ed7ea45f4f0aa08eec7ca660361e
ksg721 <gre20018@byui.edu>
2026-02-19 16:40:34 -0700

    Completed Learning 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Kyle Green", "Essay");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment assignment2 = new MathAssignment("Kyle Green", "Long Division", "4", "26-52");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+
+        WritingAssignment assignment3 = new WritingAssignment("Kyle Green", "Change Essay", "Who I Am Now");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
     }
 }
