4c820c0f6c8c16b4c2f1408b7e9ca033bf0bab9c
ksg721 <gre20018@byui.edu>
2026-03-05 13:24:41 -0700

    Completed Learning 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Green", 12);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Yellow", 3, 6.21);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Brown", 2);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
